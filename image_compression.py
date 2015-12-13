4e83fc04fa62841d396e3cbb36856607010548f2
Andrew Chen <deweichen47@gmail.com>
2015-12-12 21:52:16 -0500

    almost done

--- image_compression.py
+++ image_compression.py
@@ -15,14 +15,66 @@ from PIL import Image
 from skimage import io, img_as_float, img_as_uint
 from skimage.measure import structural_similarity as ssim
 
-def svd_compress_ssim(img, ssim):
-	"""Compress image by finding k that is closest to ssim"""
+def combine(U, S, V):
+	return np.dot(np.dot(U, S), V)
+
+def svd_compress_ssim(img, target_ss):
+	"""Compress image by finding k that is closest to target ssim.
+	Since rank and ssim relationship is linear, we do a 
+	binary search, followed by finer grained linear search"""
+	rank = min(img.shape[0], img.shape[1])
+	left = 1
+	right = rank
+	last_ss = 100
+	k = 1
+	compressed = None
 	U, singular_vals, V = linalg.svd(img)
-	rank = len(singular_vals)
-	if k > rank:
-		print "k is larger than rank of image %r" % rank
-		return img
-	pass
+	# binary search
+	while left < right:	
+		k = (left + right) / 2
+		S_p = np.zeros((k, k), img.dtype)
+		for i in range(k):
+			S_p[i][i] = singular_vals[i]
+		compressed = combine(U[:,:k], S_p, V[:k,:])
+		ss = ssim(img, compressed,
+			dynamic_range=compressed.max()-compressed.min())
+		if abs(ss - target_ss) < abs(last_ss - target_ss):
+			last_ss = ss
+			if ss > target_ss:
+				right = k
+			else:
+				left = k
+		else:
+			break
+	# more fine grained linear search
+	if last_ss < target_ss:
+		while 1:
+			S_p = np.zeros((k + 1, k + 1), img.dtype)
+			for i in range(k + 1):
+				S_p[i][i] = singular_vals[i]
+			compressed = combine(U[:,:k+1], S_p, V[:k+1,:])
+			ss = ssim(img, compressed,
+				dynamic_range=compressed.max()-compressed.min())
+			if abs(ss - target_ss) < abs(last_ss - target_ss):
+				last_ss = ss
+				k += 1	
+			else:
+				break
+	else:
+		while 1:
+			S_p = np.zeros((k - 1, k - 1), img.dtype)
+			for i in range(k - 1):
+				S_p[i][i] = singular_vals[i]
+			compressed = combine(U[:,:k-1], S_p, V[:k-1,:])
+			ss = ssim(img, compressed,
+				dynamic_range=compressed.max()-compressed.min())
+			if abs(ss - target_ss) < abs(last_ss - target_ss):
+				last_ss = ss
+				k -= 1
+			else:
+				break	
+	print "Best k found %r with ssim %r" % (k, last_ss)
+	return compressed
 
 def svd_compress_gs(img, k):
 	"""Given a matrix representing a grayscale image, compress 
@@ -93,7 +145,7 @@ def compress_ratio(orig_img, k):
 		comp_bytes = k[0] * (m + n + 1)
 		return comp_bytes / (m * n)
 
-def compress_images(in_dir, out_dir, num_imgs, img_format, k_values):
+def compress_images_k(in_dir, out_dir, num_imgs, img_format, k_values):
 	"""Compress images with different k values. Compressed format is png."""
 	for i in range(1, num_imgs+1):
 		img = io.imread("{0}/{1}.{2}".format(in_dir, i, img_format))
@@ -109,12 +161,25 @@ def compress_images(in_dir, out_dir, num_imgs, img_format, k_values):
 				compressed = svd_compress_gs(img, k)
 			io.imsave(filename, compressed)
 
+def compress_images_ss(in_dir, out_dir, num_imgs, img_format, ss_values):
+	"""Compress images with different k values. Compressed format is png."""
+	for i in range(1, num_imgs+1):
+		img = io.imread("{0}/{1}.{2}".format(in_dir, i, img_format))
+		img = img_as_float(img)
+		for ss in ss_values:
+			filename = "{0}/{1}/{2}.png".format(out_dir, i, str(ss).replace('.', ''))
+			print filename
+			# check if this is an RGB or grayscale image
+			compressed = svd_compress_ssim(img, ss)
+			io.imsave(filename, compressed)
+
 def main():
 	parser = argparse.ArgumentParser(description='Image compression with SVD')
 	parser.add_argument('-c', dest='compress', nargs='?', help='compress image using SVD')
 	parser.add_argument('-k', dest='k', nargs='*', default=['5', '5', '5'], help='compression factor k (default 5)')
 	parser.add_argument('-s', dest='ssim', nargs=2, help='calculate ssim between 2 images')
 	parser.add_argument('-r', dest='size', type=int, default=100, help='image resize percentage (default 100)')
+	parser.add_argument('-f', dest='fname', nargs='?', help='saved compressed image to file')
 	args = parser.parse_args()
 	args.k = [int(x) for x in args.k]
 
@@ -138,9 +203,10 @@ def main():
 		else:
 			compressed = svd_compress_gs(img, args.k[0])
 		print "Compression ratio: %r" % compress_ratio(img, args.k)
+		if args.fname:
+			io.imsave(args.file, compressed)
 		io.imshow(compressed)
 		io.show()
-		io.imsave("testing.png", compressed)
 	elif args.size < 100:
 		print "Resizing image to {0}%".format(args.r)
 		img = misc.imresize(img, args.size)
@@ -152,5 +218,10 @@ def main():
 
 if __name__ == '__main__':
 	main()
-	# compress_images("../gs", "../gsdiffk", 10, "pgm", [1, 3, 5, 10, 15, 20])
-	# compress_images("../rgb", "../rgbdiffk", 10, "jpg", [5, 15, 25, 35, 45])
+
+	# compress images with different k values
+	# compress_images_k("../gs", "../gsdiffk", 10, "pgm", [1, 3, 5, 10, 15, 20])
+	# compress_images_k("../rgb", "../rgbdiffk", 10, "jpg", [5, 15, 25, 35, 45])
+
+	# compress images with different target ss values using svd_compress_ssim
+	# compress_images_ss("../gs", "../gsdiffss", 10, "pgm", [0.5, 0.6, 0.7, 0.8, 0.9])
